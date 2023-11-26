2eeada472a34c37ef0fc54da673e1016597d88dc
Prempreet Brar <prempreet.brar@ucalgary.ca>
2023-11-25 18:01:56 -0700

    define all applicant tables

--- server/models/applicantModel.js
+++ server/models/applicantModel.js
@@ -26,5 +26,102 @@ Applicant.belongsTo(User, {
   onUpdate: "CASCADE",
 });
 
+// ------------------------------------------------------------------------------------------------------------------
+
+const ApplicantExperience = sequelize.define(
+  "APPLICANT_EXPERIENCE",
+  {
+    ApplicantUsername: {
+      type: DataTypes.STRING(32),
+      primaryKey: true,
+      allowNull: false,
+    },
+    Experience: {
+      type: DataTypes.STRING(32),
+      primaryKey: true,
+      allowNull: false,
+    },
+    ExperienceDesc: {
+      type: DataTypes.TEXT,
+      comment: "Optional longer description of experience",
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+// Define the foreign key relationship
+ApplicantExperience.belongsTo(Applicant, {
+  foreignKey: "ApplicantUsername",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+// ------------------------------------------------------------------------------------------------------------------
+
+const ApplicantProject = sequelize.define(
+  "APPLICANT_PROJECT",
+  {
+    ApplicantUsername: {
+      type: DataTypes.STRING(32),
+      primaryKey: true,
+      allowNull: false,
+    },
+    Project: {
+      type: DataTypes.STRING(32),
+      primaryKey: true,
+      allowNull: false,
+    },
+    ProjectDesc: {
+      type: DataTypes.TEXT,
+      comment: "Optional longer description of project",
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+// Define the foreign key relationship
+ApplicantProject.belongsTo(Applicant, {
+  foreignKey: "ApplicantUsername",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+// ------------------------------------------------------------------------------------------------------------------
+
+const ApplicantCertification = sequelize.define(
+  "APPLICANT_CERTIFICATION",
+  {
+    ApplicantUsername: {
+      type: DataTypes.STRING(32),
+      primaryKey: true,
+      allowNull: false,
+    },
+    Certification: {
+      type: DataTypes.STRING(32),
+      primaryKey: true,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+// Define the foreign key relationship
+ApplicantCertification.belongsTo(Applicant, {
+  foreignKey: "ApplicantUsername",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+// ------------------------------------------------------------------------------------------------------------------
+
 sequelize.sync();
-module.exports = Applicant;
+exports.Applicant = Applicant;
+exports.ApplicantExperience = ApplicantExperience;
+exports.ApplicantProject = ApplicantProject;
+exports.ApplicantCertification = ApplicantCertification;
