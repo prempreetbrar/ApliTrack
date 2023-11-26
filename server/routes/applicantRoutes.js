eebe2818b7c01afada2e9a450b9d25f2416a0159
Prempreet Brar <prempreet.brar@ucalgary.ca>
2023-11-25 20:12:16 -0700

    Check if applicant logged in before welcoming them

--- server/routes/applicantRoutes.js
+++ server/routes/applicantRoutes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const applicantController = require("../controllers/applicantController");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
-router.post("/get-started", applicantController.createApplicant);
+router.post(
+  "/get-started",
+  authController.checkIfLoggedIn,
+  applicantController.createApplicant
+);
 
 module.exports = router;
