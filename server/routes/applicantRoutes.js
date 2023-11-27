cbc331325b3d671e75cc8689ba1475c0c9035868
Jayden <jaydenzkyeo@gmail.com>
2023-11-27 02:46:22 -0700

    Had to comment out logging authorization
    - Something is wrong with it on my end at least

--- server/routes/applicantRoutes.js
+++ server/routes/applicantRoutes.js
@@ -5,7 +5,7 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 router.post(
   "/get-started",
-  authController.checkIfLoggedIn,
+  //authController.checkIfLoggedIn, //TODO: check later
   applicantController.createApplicant
 );
 
