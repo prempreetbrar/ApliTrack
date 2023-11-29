f6ccbfd7d1c8a19dc4bfa5befe77459988c84d2c
kliker1228 <fredprokopchuk999@gmail.com>
2023-11-28 22:04:42 -0700

    All Offer functionality; yet to test cascade on Applicant deletion

--- server/routes/offerRoutes.js
+++ server/routes/offerRoutes.js
@@ -4,8 +4,18 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.route("/details")
-.post(authController.checkIfLoggedIn, offerController.createOffer);
 
+router.route("/details")
+.post(authController.checkIfLoggedIn, offerController.createOffer)
+.delete(authController.checkIfLoggedIn, offerController.deleteOffer)
+.put(authController.checkIfLoggedIn, offerController.updateOffer);
+
+// leaving this here in case auth doesn't work
+/* 
+router.route("/details")
+.post(offerController.createOffer)
+.delete(offerController.deleteOffer)
+.put(offerController.updateOffer);
+*/
 
 module.exports = router;
