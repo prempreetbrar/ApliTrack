e0ab9d04d6ed616f15ab74971597b0c6a7164da6
Jayden <jaydenzkyeo@gmail.com>
2023-11-29 18:33:39 -0700

    cleanup

--- server/models/submitWithModel.js
+++
@@ -1,5 +0,0 @@
-const { DataTypes, Sequelize } = require("sequelize");
-const sequelize = require("../server");
-const { Applicant } = require("./applicantModel");
-const { Document } = require("./documentModel");
-const { Application } = require("./applicationModel");
