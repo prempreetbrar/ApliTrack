97bd1da19eb8ff0ccf266059513a691272adc06d
Jayden <jaydenzkyeo@gmail.com>
2023-11-30 12:33:40 -0700

    cleanup on dist

--- server/dist/controllers/documentController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const factory = require("./controllerFactory");
-const Document = require("../models/documentModel");
-exports.createDocument = factory.createOne(Document.Document);
-exports.deleteDocument = factory.deleteInstance(Document.Document);
-exports.updateDocument = factory.updateInstance(Document.Document);
