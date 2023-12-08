c00daa35055e4a7c8ef91ceb4e1c219bb0861b91
Jayden <jaydenzkyeo@gmail.com>
2023-12-08 13:53:31 -0700

    moving get all interviews route so there is no need to log in

--- server/routes/interviewRoutes.ts
+++ server/routes/interviewRoutes.ts
@@ -3,12 +3,13 @@ const authController = require("../controllers/authController");
 const interviewController = require("../controllers/interviewController");
 
 const router = express.Router();
+
+router.route("").get(interviewController.getAllInterviews);
+
 /*
     For any route that comes after this, we will check if the user is loggedIn.
 */
 router.use(authController.checkIfLoggedIn);
-
-router.route("").get(interviewController.getAllInterviews);
 
 router
   .route("/details")
