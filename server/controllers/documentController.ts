b94746fe6e7b9dc02fd58c318fcfa4a8319721d5
Jayden <jaydenzkyeo@gmail.com>
2023-12-08 13:53:05 -0700

    Added get requests and filters for documents

--- server/controllers/documentController.ts
+++ server/controllers/documentController.ts
@@ -1,6 +1,29 @@
 const factory = require("./controllerFactory");
 const Document = require("../models/documentModel");
+const errorHandling = require("../utils/errorHandling");
 
 exports.createDocument = factory.createOne(Document.Document);
 exports.deleteDocument = factory.deleteInstance(Document.Document);
 exports.updateDocument = factory.updateInstance(Document.Document);
+
+exports.getDocument = factory.getOne(Document.Document);
+exports.getAllDocuments = factory.getAll(Document.Document);
+exports.getAllApplicantDocuments = factory.getAll(Document.Document);
+
+exports.filterID = errorHandling.catchAsync(
+    async (request, response, next) => {
+      request.body.filter = {
+        DocumentID: request.body.DocumentID,
+      };
+      next();
+    }
+  );
+
+  exports.filterApplicant = errorHandling.catchAsync(
+    async (request, response, next) => {
+      request.body.filter = {
+        ApplicantUsername: request.body.ApplicantUsername,
+      };
+      next();
+    }
+  );
